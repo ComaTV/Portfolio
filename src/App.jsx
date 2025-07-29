e1bb278a21a6bf27bda3da6f2c7b4cb5772e24b6
ComaTV <comatvc@gmail.com>
2025-07-29 03:25:56 +0300

    quick setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Button, Container, ContainerExample } from 'mc-ui-comatv';
+
+function App() {
+  return (
+    <div className='bg-black h-screen'>
+      <Container 
+        width="300px"
+        height="200px"
+        fontFamily="MinecraftTen"
+        padding="20px"
+        variant="card"
+        size="large"
+      >
+        Continutul tau aici
+      </Container>
+    </div>
+  );
+}
+
+export default App;
